fb683d3d7472e7456daccd7a22c55358a43201b7
S11as <50956430+S11as@users.noreply.github.com>
2021-01-31 17:31:28 +0300

    slider

---
+++ src/components/Slider/Slider.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="hello">
+    <swiper
+      :slides-per-view="1"
+      :space-between="50"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+      :autoplay="true"
+      :class="styles.heroMargin"
+    >
+      <swiper-slide>
+        <img :src="slide1" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img :src="slide1" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img :src="slide1" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img :src="slide1" alt="">
+      </swiper-slide>
+    </swiper>
+    <img :src="trying" alt="" :class="styles.sliderBg">
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import SwiperCore, { Pagination, Autoplay } from 'swiper'
+
+import styles from 'Sass/slider.module.sass'
+import 'swiper/swiper.min.css'
+import 'swiper/components/autoplay'
+import 'swiper/components/pagination/pagination.min.css'
+
+import slide1 from 'Assets/slider/slide1.png'
+import trying from 'Assets/try.png'
+
+SwiperCore.use([Pagination, Autoplay])
+
+export default {
+  name: 'Slider',
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  data () {
+    return {
+      slider: null,
+      slideCount: 3,
+      styles,
+      slide1,
+      trying
+    }
+  },
+  mounted () {
+    let currentSlide = 1
+    let slideDirection = true
+    setInterval(() => {
+      try {
+        if (this.slider) {
+          if (slideDirection) {
+            this.slider.slideNext(1000)
+            currentSlide++
+            if (currentSlide === this.slideCount) slideDirection = false
+          } else {
+            this.slider.slidePrev(1000)
+            currentSlide--
+            if (currentSlide === 1) slideDirection = true
+          }
+        }
+      } catch (e) {}
+    }, 3500)
+  },
+  methods: {
+    onSwiper (swiper) {
+      this.slider = swiper
+    },
+    onSlideChange () {
+      console.log('slide change')
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,49 @@
 <template>
   <section>
-    <img :src="hero" alt="minecraft background" :class="hero">
+    <img :src="hero" alt="minecraft background" :class="styles.hero">
+    <div class="container" :class="styles.wrapper">
+      <div class="row justify-content-between">
+        <div class="col-auto">
+          <div class="row">
+            <img :src="logo" alt="foxcraft logo" :class="styles.logo">
+          </div>
+          <div class="row justify-content-center">
+            <button class="col-auto" :class="styles.heroButton">
+              <img :src="pickaxe1">
+              <span :class="styles.heroButtonText">Начать игру</span>
+              <img :src="pickaxe2">
+            </button>
+          </div>
+        </div>
+        <div class="col-6">
+          <Slider/>
+        </div>
+      </div>
+    </div>
   </section>
+
 </template>
 
 <script>
 import styles from 'Sass/home.module.sass'
 import hero from '@/assets/hero-background.png'
+import logo from 'Assets/logo.png'
+import pickaxe1 from 'Icons/pickaxe1.svg'
+import pickaxe2 from 'Icons/pickaxe2.svg'
+import Slider from '@/components/Slider/Slider'
 
 export default {
   name: 'Home',
+  components: {
+    Slider
+  },
   setup () {
     return {
       styles,
-      hero
+      hero,
+      logo,
+      pickaxe1,
+      pickaxe2
     }
   }
 }
